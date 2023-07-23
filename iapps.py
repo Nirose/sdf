c68e4a58e9175076e56ac368321c9c3007354687
Jorin Manandhar <nirose@jucktion.com>
2023-07-23 09:58:46 +0545

    more cmd

--- iapps.py
+++ iapps.py
@@ -62,6 +62,7 @@ class apptoForum:
             self.d = webdriver.Chrome(service=s, options=options)
         else:
             from webdriver_manager.chrome import ChromeDriverManager
+            from webdriver_manager.core.utils import ChromeType
             self.d = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
         self.d.implicitly_wait(2)
         print("Chrome Browser Invoked")
--- kindle.py
+++ kindle.py
@@ -89,6 +89,7 @@ class booktoForum:
             self.d = webdriver.Chrome(service=s, options=options)
         else:
             from webdriver_manager.chrome import ChromeDriverManager
+            from webdriver_manager.core.utils import ChromeType
             self.d = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
 
     def stop(self):
